08ec9623ef75867e4079e7462147dadfff82576b
AwdeDarkar <darkardengeno@gmail.com>
2017-09-05 22:14:16 -0400

    Began event handling system

---
+++ Events/events.py
@@ -0,0 +1,34 @@
+class AbstractEvent:
+    def __init__(self):
+        self.listeners = []
+    
+    def __str__(self):
+        return "abstract event"
+    
+    def __repr__(self):
+        return self.__str__()
+    
+    #The idea is that an event object can be 'called' like a function. This might be terrible.
+    def __call__(self, *args):
+        #Maybe add a log option here
+        for listener in self.listeners:
+            listener(self, *args)
+    
+    def listen(self, listener):
+        self.listeners.append(listener)
+    
+    def stopListening(self, listener):
+        self.listeners.remove(listener)
+
+class CentralEventManager:
+    def __init__(self):
+        self.eventPollPair = []
+    
+    def addEvent(self, event, poll):
+        self.eventPollPair.append((event, poll))
+    
+    def tick(self):
+        for pair in self.eventPollPair:
+            if(pair[1]()):
+                pair[0]()
+
